4808086406e9c8321b117089480634887c020c22
Vworks <dpenlan1@kent.edu>
2021-11-04 06:03:20 -0400

    Moved over buttons, image button still needs to be finished

--- src/fields/buttons.tsx
+++ src/fields/buttons.tsx
@@ -1,27 +1,97 @@
 import React from 'react';
+import '../vislib.module.css';
 
-export const Normal = ({id, text, icon, className, active, disabled}: any) => {
-    let setClassName = active == true ? `btn btn-light pda-btn ${className ? className : null} active` : `btn btn-light pda-btn ${className ? className : null}`;
+export const Normal = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
 
     return(
-        <button className={setClassName} id={id} {...disabled ? 'disabled' : null}>
+        <button className={`btn btn-light ${setClassName}`} id={id} disabled={setDisabled}>
             {icon}{text}
         </button>
     );
 };
 
 export const Previous = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+    icon = icon ? icon : <i className="bi bi-arrow-bar-left"></i>;
+
     return(
-        <button className={className} id={id} {...disabled ? 'disabled' : null}>
+        <button className={`btn btn-light ${setClassName}`} id={id} disabled={setDisabled}>
             {icon}{text}
         </button>
     );
 };
 
-export const Next = ({}) => {};
-export const Primary = ({}) => {};
-export const Warning = ({}) => {};
-export const Danger = ({}) => {};
-export const Add = ({}) => {};
-export const Delete = ({}) => {};
-export const Icon = ({}) => {};
+export const Next = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+    icon = icon ? icon : <i className="bi bi-arrow-bar-right"></i>;
+    
+    return(
+        <button className={`btn btn-light ${setClassName}`} id={id} disabled={setDisabled}>
+            {text}{icon}
+        </button>
+    );
+};
+
+export const Primary = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+
+    return(
+        <button className={`btn btn-primary ${setClassName}`} id={id} disabled={setDisabled}>
+            {icon}{text}
+        </button>
+    );
+};
+
+export const Warning = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+
+    return(
+        <button className={`btn btn-warning ${setClassName}`} id={id} disabled={setDisabled}>
+            {icon}{text}
+        </button>
+    );
+};
+
+export const Danger = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+
+    return(
+        <button className={`btn btn-danger ${setClassName}`} id={id} disabled={setDisabled}>
+            {icon}{text}
+        </button>
+    );
+};
+
+export const Add = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+    icon = <i className="bi bi-person-plus-fill"></i>;
+
+    return(
+        <button className={`btn btn-light ${setClassName}`} id={id} disabled={setDisabled}>
+            {icon}{text}
+        </button>
+    );
+};
+
+export const Delete = ({id, text, icon, className, disabled}: any) => {
+    let setClassName = className ? className : "";
+    let setDisabled = disabled ? true : undefined;
+    icon = <i className="bi bi-trash-fill"></i>;
+
+    return(
+        <button className={`btn btn-danger ${setClassName}`} id={id} disabled={setDisabled}>
+            {icon}{text}
+        </button>
+    );
+};
+// export const Icon = ({id, text, icon, className, disabled}: any) => {
+//     let iconImage = <img src={props.src}></img>;
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Normal } from './fields/buttons'
+import {Add, Danger, Delete, Next, Normal, Previous, Primary, Warning } from './fields/buttons'
 import styles from './styles.module.css'
 
 interface Props {
@@ -8,6 +8,13 @@ interface Props {
 
 export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}
-  <Normal id="test btn" text="button"/>
+  <Normal text="button" />
+  <Previous id="test btn" text="previous" />
+  <Next id="test btn" text="next" />
+  <Warning id="test btn" text="warn" />
+  <Danger id="test btn" text="danger" />
+  <Add id="test btn" text="add" />
+  <Primary id="test btn" text="prime" />
+  <Delete id="test btn" text="delete" />
   </div>
 }
